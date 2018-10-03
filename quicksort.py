ca5ecc23c81465389b052f658f4ebb1af838f171
UpAndDownAgain <musil.adam@gmail.com>
2018-10-03 15:05:46 +0200

    working sort

--- quicksort.py
+++ quicksort.py
@@ -1,15 +1,14 @@
 from random import randint
 
 def quicksort(array):
-    if len(array) > 2:
+    if len(array) < 2:
         return array
     else:
-        pivot = array[randint(0, len(array)-1)]
+        pivot = array[randint(0,len(array)-1)]
         less = [i for i in array if i < pivot]
         equal = [i for i in array if i == pivot]
         more = [i for i in array if i > pivot]
-        return quickort(less) + equal + quicksort(more)
+        return quicksort(less) + equal + quicksort(more)
 
 arr = [1,4,56,4,8,5,6,74,1,23,3,54]
-
 print(quicksort(arr))
